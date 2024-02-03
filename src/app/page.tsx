32dfd151d860473900675396def5349f394e562f
Akshat Raj <akshatm196@gmail.com>
2024-02-03 03:17:35 -0800

    transforming niwax into next js

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,16 +3,34 @@ import Feature from '@/Components/Feature'
 import HeroSection from '@/Components/HeroSection'
 import Navbar from '@/Components/NavBar'
 import Slider from '@/Components/Slides'
-import Image from 'next/image'
 
+import React from 'react'
+import "bootstrap/dist/css/bootstrap.min.css";
+import Image from 'next/image'
+import Hero from '@/Components/Hero'
+import Topfeature from '@/Components/Topfeature'
+import Feature2 from '@/Components/Feature2'
+import Stats from '@/Components/Stats';
+
+import Feature3 from '@/Components/Feature3';
+import OurFeature from '@/Components/OurFeature';
+import OurCustomer from '@/Components/OurCustomer';
+import Partner from '@/Components/Partner';
+import Footer from '@/Components/Footer';
 export default function Home() {
   return (
  
     <>
-       <HeroSection/>
-       {/* <Slider/> */}
-       <Feature/>
-       <Contact/>
+        
+    <Hero/>
+    <Topfeature/>
+    <Feature2/>
+    <Stats/>
+    <Feature3/>
+    <OurFeature/>
+    <OurCustomer/>
+    <Partner/>
+    <Footer/>
     </>
  
   )
